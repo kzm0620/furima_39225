3645436e20c895ec46e825e2b8278976c0d1d631
kzm0620 <kzm.staygold@gmail.com>
2023-05-11 14:59:46 +0900

    ユーザー管理機能　修正１

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    first_name            {Faker::Name.initials(number: 2)}
+    last_name             {Faker::Name.initials(number: 2)}
+    first_name_kana       {Faker::Name.initials(number: 2)}
+    last_name_kana        {Faker::Name.initials(number: 2)}
+    birth_date            {Faker::Date.backward}
   end
 end
