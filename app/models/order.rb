ee86ce7b5af54812a0c824bb954e8778ecae028b
kzm0620 <kzm.staygold@gmail.com>
2023-05-26 16:27:31 +0900

    修正４

--- app/models/order.rb
+++ app/models/order.rb
@@ -14,8 +14,8 @@ class Order
     validates   :token
   end
 
-    validates   :prefecture_id,      numericality:  {other_than: 0, message: "can't be blank"}
-  
+    validates   :prefecture_id,      numericality:  {other_than: 1, message: "can't be blank"}
+
   
   
   def save
