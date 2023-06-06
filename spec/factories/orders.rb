912dbf9a501a4837c4acb84d4c44bbd9d6477683
kzm0620 <kzm.staygold@gmail.com>
2023-06-06 11:10:28 +0900

    追加実装　修正１

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :order do
     token              {"tok_abcdefghijk00000000000000000"}
     post_code          {'123-4567'}
-    prefecture_id      {'1'}
+    prefecture_id      {'2'}
     municipalities     {"東京都"}
     address            {'1-1'}
     building_name      {'東京ハイツ'}
